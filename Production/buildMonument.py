8e55c8a829ef8a6e40d9d40d074e015cc516451d
Andy Clymer <git@andyclymer.com>
2019-11-03 17:21:02 -0500

    build script change

--- Production/buildMonument.py
+++ Production/buildMonument.py
@@ -1,8 +1,8 @@
 from RotateMaster import buildDesignSpace
 
 buildDesignSpace(
-    masterPath="/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Monument/Masters/Tilt-Monument-SmallTest.ufo", 
-    destPath="/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Monument/Rotated Test", 
+    masterPath="/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Monument/Masters/Tilt-Monument.ufo", 
+    destPath="/Users/clymer/Documents/Code/Git repos/GitHub/andyclymer/Tilt-Typeface/sources/Tilt Monument/Rotated", 
     glyphNames=[],
     compositionType="rotate", 
     outlineAmount=10, 
