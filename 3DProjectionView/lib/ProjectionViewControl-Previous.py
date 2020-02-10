b35c0974b0845b2bad6bc42ed20dcc9f0dd3f57a
Andy Clymer <hello@andyclymer.com>
2020-02-10 14:57:21 -0500

    Reworking Projection View Control's UI

--- 3DProjectionView/lib/ProjectionViewControl-Previous.py
+++ 3DProjectionView/lib/ProjectionViewControl-Previous.py
@@ -1,21 +1,9 @@
+from roboHUD import BaseRoboHUDControl, registerControlClass, RoboHUDController
 from mojo.events import addObserver, removeObserver
-from mojo.canvas import CanvasGroup
 import vanilla
 import math
 import random
 import copy
-
-
-"""
-TO DO:
-
-    Only have one instance of this running, and don't rebuild the widget on each new window opening.
-    The state of the checkbox needs to remain on/off during the session, keep it as one object
-
-    === Make a separate object for the UI, and then just attach that to the window whenever it's opened
-
-"""
-
 
 
 def makeUniqueName(length=None):
@@ -33,22 +21,23 @@ def getSetUniqueName(pt):
     
 def interpolate(f, x0, x1):
     return x0 + (x1 - x0) * f
+    
 
-
-
-
-class ProjectionViewControl(object):
+class ProjectionViewControl(BaseRoboHUDControl):
     
     """
     - glyph lib stores the "z" location under each point name
     - self.pointData holds a dict of "x,y,z" for each point name
     """
 
-    def __init__(self):
+    name = "Projection View"
+    size = (95, 95)
+    dimWhenInactive = False
+
+    def start(self):
+        super(ProjectionViewControl, self).start()
         
         self.debug = False
-        
-        self.view = None
         
         # Glyph and font data
         self.glyph = None
@@ -61,22 +50,7 @@ class ProjectionViewControl(object):
         self.currentView = "front"
         self.enableProjection = False # Enable the buttons and save point data to the lib?
         
-        addObserver(self, "observerGlyphWindowWillOpen", "glyphWindowWillOpen")
-        addObserver(self, "observerDraw", "draw")
-        addObserver(self, "observerDrawPreview", "drawPreview")
-        
-        addObserver(self, "glyphWillChange", "viewWillChangeGlyph")
-        addObserver(self, "glyphDidChange", "viewDidChangeGlyph")
-        addObserver(self, "fontWillSave", "glyphWindowWillClose")
-        addObserver(self, "fontWillSave", "fontWillSave")
-        addObserver(self, "fontDidSave", "fontDidSave")
-        addObserver(self, "mouseUp", "mouseUp")
-        
-        
-    def observerGlyphWindowWillOpen(self, notification):
-        self.window = notification["window"]
         # Window controls
-        self.view = CanvasGroup((30, 30, 95, 95), delegate=self)
         self.view.controlGroup = vanilla.Box((0, 0, 100, 100))
         self.view.controlGroup.buttonFront = vanilla.SquareButton((40, 5, 40, 40), "●", callback=self.rotateFront)
         self.view.controlGroup.buttonFront.enable(False)
@@ -85,49 +59,52 @@ class ProjectionViewControl(object):
         self.view.controlGroup.buttonTop = vanilla.SquareButton((40, 50, 40, 30), "◓", callback=self.rotateTop)
         self.view.controlGroup.buttonTop.enable(False)
         self.view.controlGroup.enableBox = vanilla.CheckBox((13, -38, 25, 25), "", callback=self.enableDisableCallback)
-        # add the view to the GlyphEditor window
-        self.window.addGlyphEditorSubview(self.view)
-            
-    def observerDraw(self, notification):
-        # Show the controls
-        if self.view:
-            self.view.show(True)
         
-    def observerDrawPreview(self, notification):
-        # Hide the controls in preview mode
-        if self.view:
-            self.view.show(False)
+        addObserver(self, "glyphWillChange", "viewWillChangeGlyph")
+        addObserver(self, "glyphDidChange", "viewDidChangeGlyph")
+        addObserver(self, "fontWillSave", "glyphWindowWillClose")
+        addObserver(self, "fontWillSave", "fontWillSave")
+        addObserver(self, "fontDidSave", "fontDidSave")
+        addObserver(self, "mouseUp", "mouseUp")
 
+    def stop(self):
+        super(ProjectionViewControl, self).stop()
+        removeObserver(self, "viewWillChangeGlyph")
+        removeObserver(self, "viewDidChangeGlyph")
+        removeObserver(self, "glyphWindowWillClose")
+        removeObserver(self, "fontWillSave")
+        removeObserver(self, "fontDidSave")
+        removeObserver(self, "fontResignCurrent")
+        removeObserver(self, "fontBecameCurrent")
+    
     
     # Window Callbacks
     
     def enableDisableCallback(self, sender):
         # Enable/disable the projection view
         # When disabled, rotate back to the front and stop reading/writing lib data
-        if self.view:
-            self.enableProjection = self.view.controlGroup.enableBox.get()
-            if self.enableProjection:
-                self.libReadGlyph()
-            self._updateXYZPointData() # Organize the point data
-            if not self.enableProjection:
-                self.libWriteGlyph()
-            self.rotate("front")
-            self.updateButtons()
+        self.enableProjection = self.view.controlGroup.enableBox.get()
+        if self.enableProjection:
+            self.libReadGlyph()
+        self._updateXYZPointData() # Organize the point data
+        if not self.enableProjection:
+            self.libWriteGlyph()
+        self.rotate("front")
+        self.updateButtons()
     
     def updateButtons(self):
         # Enable/disable buttons to match the current state
-        if self.view:
-            self.view.controlGroup.buttonFront.enable(self.enableProjection)
-            self.view.controlGroup.buttonSide.enable(self.enableProjection)
-            self.view.controlGroup.buttonTop.enable(self.enableProjection)
-            self.view.controlGroup.enableBox.set(self.enableProjection)
-            if self.enableProjection:
-                if self.currentView == "front":
-                    self.view.controlGroup.buttonFront.enable(False)
-                if self.currentView == "side":
-                    self.view.controlGroup.buttonSide.enable(False)
-                if self.currentView == "top":
-                    self.view.controlGroup.buttonTop.enable(False)
+        self.view.controlGroup.buttonFront.enable(self.enableProjection)
+        self.view.controlGroup.buttonSide.enable(self.enableProjection)
+        self.view.controlGroup.buttonTop.enable(self.enableProjection)
+        self.view.controlGroup.enableBox.set(self.enableProjection)
+        if self.enableProjection:
+            if self.currentView == "front":
+                self.view.controlGroup.buttonFront.enable(False)
+            if self.currentView == "side":
+                self.view.controlGroup.buttonSide.enable(False)
+            if self.currentView == "top":
+                self.view.controlGroup.buttonTop.enable(False)
     
     
     # Observer Callbacks
@@ -153,37 +130,35 @@ class ProjectionViewControl(object):
             self.rotate(self.currentView)
     
     def glyphWillChange(self, info):
-        if self.view:
-            if self.debug: print("glyphWillChange", info["glyph"])
-            # If there was already a glyph, remove the observer, rotate to the front and save
-            if not self.glyph == None:
-                self.glyph.removeObserver(self, "Glyph.Changed")
-                self.glyph.removeObserver(self, "Glyph.LibChanged")
-                self._updateXYZPointData()
-                self.rotate("front")
-                self.libWriteGlyph()
-            self.glyph = None
-            self.pointData = {}
-            # Disable the projection view, until the new glyph enables it
-            self.view.controlGroup.enableBox.set(0)
-            self.enableProjection = False
-            self.updateButtons()
+        if self.debug: print("glyphWillChange", info["glyph"])
+        # If there was already a glyph, remove the observer, rotate to the front and save
+        if not self.glyph == None:
+            self.glyph.removeObserver(self, "Glyph.Changed")
+            self.glyph.removeObserver(self, "Glyph.LibChanged")
+            self._updateXYZPointData()
+            self.rotate("front")
+            self.libWriteGlyph()
+        self.glyph = None
+        self.pointData = {}
+        # Disable the projection view, until the new glyph enables it
+        self.view.controlGroup.enableBox.set(0)
+        self.enableProjection = False
+        self.updateButtons()
     
     def glyphDidChange(self, info):
-        if self.view:
-            if self.debug: print("glyphDidChange", info["glyph"])
-            # If there is a new glyph, get set up and read lib data
-            if not self.glyph == info["glyph"]:
-                self.glyph = info["glyph"]
-                if not self.glyph == None:
-                    self.glyph.addObserver(self, "glyphDataChanged", "Glyph.Changed")
-                    self.glyph.addObserver(self, "glyphLibDataChanged", "Glyph.LibChanged")
-                    self.libReadGlyph()
-                    # If there is pointData, enable projection editing
-                    if len(self.pointData.keys()) > 0:
-                        self.view.controlGroup.enableBox.set(1)
-                        self.enableProjection = True
-                        self.updateButtons()
+        if self.debug: print("glyphDidChange", info["glyph"])
+        # If there is a new glyph, get set up and read lib data
+        if not self.glyph == info["glyph"]:
+            self.glyph = info["glyph"]
+            if not self.glyph == None:
+                self.glyph.addObserver(self, "glyphDataChanged", "Glyph.Changed")
+                self.glyph.addObserver(self, "glyphLibDataChanged", "Glyph.LibChanged")
+                self.libReadGlyph()
+                # If there is pointData, enable projection editing
+                if len(self.pointData.keys()) > 0:
+                    self.view.controlGroup.enableBox.set(1)
+                    self.enableProjection = True
+                    self.updateButtons()
     
     def glyphDataChanged(self, notification):
         if self.debug: print("glyphDataChanged, hold:", self.holdChanges)
@@ -209,8 +184,18 @@ class ProjectionViewControl(object):
             self.libReadGlyph()
     
 
+    
+    
     # Helper functions
+    
+    def _getNeighborOnCurves(self, point):
+        prevOnCurve = None
+        nextOnCurve = None
+        idx = point.contour.index(point)
+        print(idx)
+    # @@@@@@@@@
         
+    
     def _cleanXYZPointData(self):
         # Before most operations, a check and a fix can be done on the pointData.
         # 1) If multiple points share the same ID, assign new IDs and copy pointData entries for the new point IDs
@@ -235,7 +220,9 @@ class ProjectionViewControl(object):
                 self.libWriteGlyph()
                 self.glyph.changed()
                 
-                
+                    
+    
+    
     def _updateXYZPointData(self):
         if self.debug: print("_updateXYZPointData (glyph drawing changed)")
         # The glyph drawing changed, clean up the 3D point data
@@ -300,32 +287,31 @@ class ProjectionViewControl(object):
     def libReadGlyph(self):
         # The lib only holds the "z" position
         # Read the lib and organize it into a pointData dict that has (x, y, z)
-        if self.view:
-            if self.debug: print("libReadGlyph", self.glyph)
-            # Rotate to the front
-            if not self.currentView == "front":
-                self.rotate("front")
-            # Reset the point data dictionary
-            self.pointData = {}
-            # Read the lib data
-            if not self.glyph == None:
-                if self.LIBKEY in self.glyph.lib:
-                    libData = self.glyph.lib[self.LIBKEY]
-                else: libData = {}
-                # If there is libData, enable the projection and keep procesing
-                if len(libData.keys()):
-                    # Enable the projection, if it's not already enabled
-                    self.view.controlGroup.enableBox.set(1)
-                    self.enableProjection = True
-                    self.updateButtons()
-                    # Finish processing the data out of the lib
-                    for c in self.glyph.contours:
-                        for pt in c.points:
-                            ident = getSetUniqueName(pt)
-                            if ident in libData:
-                                zLoc = libData[ident]
-                            else: zLoc = 0
-                            self.pointData[ident] = dict(x=pt.x, y=pt.y, z=zLoc)
+        if self.debug: print("libReadGlyph", self.glyph)
+        # Rotate to the front
+        if not self.currentView == "front":
+            self.rotate("front")
+        # Reset the point data dictionary
+        self.pointData = {}
+        # Read the lib data
+        if not self.glyph == None:
+            if self.LIBKEY in self.glyph.lib:
+                libData = self.glyph.lib[self.LIBKEY]
+            else: libData = {}
+            # If there is libData, enable the projection and keep procesing
+            if len(libData.keys()):
+                # Enable the projection, if it's not already enabled
+                self.view.controlGroup.enableBox.set(1)
+                self.enableProjection = True
+                self.updateButtons()
+                # Finish processing the data out of the lib
+                for c in self.glyph.contours:
+                    for pt in c.points:
+                        ident = getSetUniqueName(pt)
+                        if ident in libData:
+                            zLoc = libData[ident]
+                        else: zLoc = 0
+                        self.pointData[ident] = dict(x=pt.x, y=pt.y, z=zLoc)
     
     def _dataCheck(self):
         # Make sure the point data looks valid
@@ -421,4 +407,4 @@ class ProjectionViewControl(object):
         self.rotate("top")
 
 
-ProjectionViewControl()
+registerControlClass(ProjectionViewControl)
